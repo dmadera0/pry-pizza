c05b4d0418e57b156beb0e2ae4bec93689d3e312
Daniel <dmadera0@gmail.com>
2020-08-19 15:39:33 -0600

    updated database

--- db/migrate/20200818222312_create_drinks_table.rb
+++ db/migrate/20200818222312_create_drinks_table.rb
@@ -1,6 +1,6 @@
 class CreateDrinksTable < ActiveRecord::Migration[6.0]
   def change
-    create_table :customers do |t|
+    create_table :drinks do |t|
       t.string :name
       t.integer :price
     end
